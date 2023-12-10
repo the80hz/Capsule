3a54de90cf2eb0131a2c57f39cdaa3afc4ed7839
the80hz <dnl.vlgv@mail.ru>
2023-12-11 00:26:20 +0400

    - фикс для создания жесткой ссылки

--- backup_manager.py
+++ backup_manager.py
@@ -21,30 +21,36 @@ def backup_files(source, destination, db_file):
             files = os.listdir(src)
             for file in files:
                 recursive_copy(os.path.join(src, file), os.path.join(dst, file))
+
         else:
             file_size = os.path.getsize(src)
             last_modified = os.path.getmtime(src)
-
+            size_flag = 0
+            file_hash = ''
             if file_size < 500 * 1024 * 1024:  # Для файлов < 500 МБ
                 file_hash = calculate_sha256(src)
                 existing_data = get_file_data(db_conn, file_hash=file_hash)
-                if not existing_data:
-                    shutil.copy(src, dst)
-                    insert_file_data(db_conn, src, file_size, last_modified, file_hash)
-                    logging.info(f"Скопирован файл: {src}")
-                else:
-                    create_hard_link(src, dst)
-                    logging.info(f"Создана жесткая ссылка для файла: {src}")
             else:  # Для файлов >= 500 МБ
+                size_flag = 1
                 existing_data = get_file_data(db_conn, size=file_size, last_modified=last_modified)
-                if not existing_data:
-                    shutil.copy(src, dst)
+            print(existing_data)
+
+            if not existing_data:
+                shutil.copy(src, dst)
+                if size_flag == 1:
                     file_hash = calculate_sha256(src)  # Вычисляем хеш для записи в БД
-                    insert_file_data(db_conn, src, file_size, last_modified, file_hash)
-                    logging.info(f"Скопирован файл: {src}")
+                insert_file_data(db_conn, src, file_size, last_modified, file_hash, dst)
+                logging.info(f"Скопирован файл: {src}")
+            else:
+                if size_flag == 1:
+                    file_hash = existing_data[1]
+                    backup_path = existing_data[3]
+                    create_hard_link(backup_path, dst)
                 else:
-                    create_hard_link(src, dst)
-                    logging.info(f"Создана жесткая ссылка для файла: {src}")
+                    backup_path = existing_data[3]
+                    create_hard_link(backup_path, dst)
+                insert_file_data(db_conn, src, file_size, last_modified, file_hash, dst)
+                logging.info(f"Создана жесткая ссылка для файла: {src}")
 
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     version_path = os.path.join(destination, timestamp)
--- database.py
+++ database.py
@@ -13,37 +13,36 @@ def create_connection(db_file):
 def create_table(conn):
     try:
         c = conn.cursor()
-        # Изменение структуры таблицы
         c.execute('''
-            CREATE TABLE IF NOT EXISTS file_data
-            (path TEXT PRIMARY KEY, size INTEGER, last_modified REAL, hash TEXT)
-        ''')
+                CREATE TABLE IF NOT EXISTS file_data
+                (path TEXT PRIMARY KEY, size INTEGER, last_modified REAL, hash TEXT, backup_path TEXT)
+            ''')
     except Exception as e:
         print(e)
 
 
-def insert_file_data(conn, file_path, size, last_modified, file_hash):
+def insert_file_data(conn, orig_path, size, last_modified, file_hash, backup_path):
     try:
         c = conn.cursor()
-        # Вставка данных файла
         c.execute('''
-            INSERT INTO file_data (path, size, last_modified, hash) 
-            VALUES (?, ?, ?, ?)
-        ''', (file_path, size, last_modified, file_hash))
+                INSERT OR REPLACE INTO file_data (path, size, last_modified, hash, backup_path) 
+                VALUES (?, ?, ?, ?, ?)
+            ''', (orig_path, size, last_modified, file_hash, backup_path))
         conn.commit()
     except Exception as e:
         print(e)
 
 
 def get_file_data(conn, file_hash=None, size=None, last_modified=None):
-    c = conn.cursor()
-    if file_hash:
-        # Получение данных файла по его хешу
-        c.execute("SELECT path, size, last_modified FROM file_data WHERE hash=?", (file_hash,))
-    elif size is not None and last_modified is not None:
-        # Получение данных файла по его размеру и дате последнего изменения
-        c.execute("SELECT path, hash FROM file_data WHERE size=? AND last_modified=?", (size, last_modified))
-    else:
-        # Возвращаем None, если не предоставлены ни хеш, ни размер с датой
-        return None
-    return c.fetchone()
+    try:
+        c = conn.cursor()
+        if file_hash:
+            c.execute("SELECT path, size, last_modified, backup_path FROM file_data WHERE hash=?", (file_hash,))
+        elif size is not None and last_modified is not None:
+            c.execute("SELECT path, hash, backup_path FROM file_data WHERE size=? AND last_modified=?",
+                      (size, last_modified))
+        else:
+            return None
+        return c.fetchone()
+    except Exception as e:
+        print(e)
