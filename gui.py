4bc905a888f50a929e5f747a6c8c1e179c84a098
the80hz <dnl.vlgv@mail.ru>
2023-12-12 00:37:35 +0400

    - добавил кнопку создать копию - если создано расписание, появилась возможность его удалить - лог можно посмотреть прямо в интерфейсе

--- gui.py
+++ gui.py
@@ -1,70 +1,59 @@
 import tkinter as tk
-# Импортируем необходимые библиотеки
-import tkinter as tk
-from tkinter import filedialog
-import shutil
+from tkinter import filedialog, messagebox, scrolledtext
 import os
+import threading
 import schedule
 import time
-from datetime import datetime
-import threading
-# Импортируем другие компоненты
+import logging
 
 from database import create_connection, create_table
 from backup_manager import backup_files
 
 
-# Функция для запуска расписания в отдельном потоке
 def start_schedule():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
+class TextHandler(logging.Handler):
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.text.configure(state='normal')
+        self.text.insert(tk.END, msg + '\n')
+        self.text.configure(state='disabled')
+        self.text.yview(tk.END)
+
+
 def setup_gui():
-    # Создаем окно
+    """ Настройка графического интерфейса пользователя. """
     window = tk.Tk()
     window.title("Backup Manager")
 
-    # Функция для выбора пути источника
+    # Настройка логгера
+    log_text = scrolledtext.ScrolledText(window, height=10, state='disabled')
+    text_handler = TextHandler(log_text)
+    logging.basicConfig(filename='backup_log.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger().addHandler(text_handler)
+
     def select_source_path():
         path = filedialog.askdirectory()
         source_path_entry.delete(0, tk.END)
         source_path_entry.insert(0, path)
-
-    # Функция для выбора пути назначения
-    def select_destination_path():
-        path = filedialog.askdirectory()
-        destination_path_entry.delete(0, tk.END)
-        destination_path_entry.insert(0, path)
-
-    # Функция для установки расписания
-    def set_schedule():
-        source = source_path_entry.get()
-        destination = destination_path_entry.get()
-        interval = int(interval_entry.get())
-
-        # Создаем подключение к базе данных в директории назначения
-        db_path = os.path.join(destination, 'backup_db.sqlite')
-        db_conn = create_connection(db_path)
-        create_table(db_conn)
-
-        # Немедленный запуск процесса копирования
-        backup_files(source, destination, db_path)
-
-        # Настройка расписания для последующих копирований
-        schedule.every(interval).minutes.do(backup_files, source, destination, db_path)
-
-        # Запускаем расписание в отдельном потоке
-        schedule_thread = threading.Thread(target=start_schedule)
-        schedule_thread.start()
-
-    # Элементы интерфейса
     tk.Label(window, text="Источник:").grid(row=0, column=0)
     source_path_entry = tk.Entry(window, width=50)
     source_path_entry.grid(row=0, column=1)
     tk.Button(window, text="Выбрать", command=select_source_path).grid(row=0, column=2)
 
+    def select_destination_path():
+        path = filedialog.askdirectory()
+        destination_path_entry.delete(0, tk.END)
+        destination_path_entry.insert(0, path)
     tk.Label(window, text="Назначение:").grid(row=1, column=0)
     destination_path_entry = tk.Entry(window, width=50)
     destination_path_entry.grid(row=1, column=1)
@@ -74,15 +63,58 @@ def setup_gui():
     interval_entry = tk.Entry(window, width=50)
     interval_entry.grid(row=2, column=1)
 
-    tk.Button(window, text="Установить расписание", command=set_schedule).grid(row=3, column=1)
+    def create_backup_now():
+        source = source_path_entry.get()
+        destination = destination_path_entry.get()
+        db_path = os.path.join(destination, 'backup_db.sqlite')
+        backup_files(source, destination, db_path)
+    tk.Button(window, text="Создать Копию", command=create_backup_now).grid(row=3, column=1)
 
-    # Запускаем GUI
+    def set_schedule():
+        try:
+            source = source_path_entry.get()
+            destination = destination_path_entry.get()
+            interval = int(interval_entry.get())
+
+            db_path = os.path.join(destination, 'backup_db.sqlite')
+            with create_connection(db_path) as db_conn:
+                create_table(db_conn)
+                backup_files(source, destination, db_path)
+
+                schedule.every(interval).minutes.do(backup_files, source, destination, db_path)
+
+                schedule_thread = threading.Thread(target=start_schedule)
+                schedule_thread.start()
+        except ValueError:
+            messagebox.showerror("Ошибка", "Интервал должен быть числом.")
+        except Exception as e:
+            messagebox.showerror("Ошибка", str(e))
+
+    def delete_schedule():
+        schedule.clear()
+        messagebox.showinfo("Информация", "Расписание удалено.")
+
+    def create_or_delete_schedule():
+        if schedule_button.cget("text") == "Создать Расписание":
+            set_schedule()
+            schedule_button.config(text="Удалить Расписание")
+        else:
+            delete_schedule()
+            schedule_button.config(text="Создать Расписание")
+
+    schedule_button = tk.Button(window, text="Создать Расписание", command=create_or_delete_schedule)
+    schedule_button.grid(row=3, column=0)
+
+    def toggle_log_window():
+        if log_text.winfo_ismapped():
+            log_text.grid_remove()
+        else:
+            log_text.grid(row=4, column=0, columnspan=3, sticky="ew")
+
+    tk.Button(window, text="Показать Лог", command=toggle_log_window).grid(row=3, column=2)
+
     window.mainloop()
 
 
-def main():
-    setup_gui()
-
-
 if __name__ == "__main__":
-    main()
+    setup_gui()
