4fa515456f6e38062058756a1bd4ea8107f1b52b
the80hz <dnl.vlgv@mail.ru>
2023-12-19 18:26:51 +0400

    - установлена иконка - добавлен трей с меню - размер окна неизменяем

--- gui.py
+++ gui.py
@@ -1,10 +1,14 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox, scrolledtext
 import os
 import threading
 import schedule
 import time
 import logging
+
+import tkinter as tk
+from tkinter import filedialog, messagebox, scrolledtext
+
+import pystray
+from PIL import Image
 
 from database import create_connection, create_table
 from backup_manager import backup_files
@@ -33,6 +37,11 @@ def setup_gui():
     """ Настройка графического интерфейса пользователя. """
     window = tk.Tk()
     window.title("Backup Manager")
+
+    window.resizable(False, False)
+
+    # Установка иконки
+    window.iconbitmap('icon.ico')
 
     # Настройка логгера
     log_text = scrolledtext.ScrolledText(window, height=10, state='disabled')
@@ -74,6 +83,7 @@ def setup_gui():
                 backup_files(source, destination, db_path)
 
         except Exception as e:
+            logging.error(f'"Ошибка", {str(e)}')
             messagebox.showerror("Ошибка", str(e))
     tk.Button(window, text="Создать Копию", command=create_backup_now).grid(row=3, column=1)
 
@@ -95,9 +105,12 @@ def setup_gui():
         except ValueError:
             messagebox.showerror("Ошибка", "Интервал должен быть числом.")
         except Exception as e:
+            logging.error(f'"Ошибка", {str(e)}')
             messagebox.showerror("Ошибка", str(e))
+        logging.info("Расписание было установлено")
 
     def delete_schedule():
+        logging.info("Расписание было удалено")
         schedule.clear()
         messagebox.showinfo("Информация", "Расписание удалено.")
 
@@ -119,6 +132,34 @@ def setup_gui():
             log_text.grid(row=4, column=0, columnspan=3, sticky="ew")
 
     tk.Button(window, text="Показать Лог", command=toggle_log_window).grid(row=3, column=2)
+
+    def exit_application(icon):
+        icon.stop()
+        delete_schedule()
+        window.destroy()
+
+    def on_clicked(icon):
+        logging.info("Icon clicked, stopping icon and showing window.")
+        icon.stop()
+        window.deiconify()  # Показать окно
+
+    def hide_window():
+        logging.info("Hiding window and starting tray icon.")
+
+        window.withdraw()
+
+        icon_image = Image.open('icon.ico')
+        menu = (
+            pystray.MenuItem('Восстановить', on_clicked),
+            pystray.MenuItem('Старт Резервной Копии', create_backup_now),
+            pystray.MenuItem('Выход', exit_application),
+        )
+        icon = pystray.Icon("name", icon_image, "title", menu)
+
+        icon.left_click_action = on_clicked
+        icon.run()
+
+    window.protocol("WM_DELETE_WINDOW", hide_window)
 
     window.mainloop()
 
